3dc119fc57ecaa76c866855bed450ede664d4a07
Niraj Trivedi <mailme7778@gmail.com>
2019-01-25 23:28:39 +1100

    Implemented MediatR and FluentValidation

--- Branch.API/Features/PingMe/PingMeController.cs
+++ Branch.API/Features/PingMe/PingMeController.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+using MediatR;
+using Branch.API.Features.PingMe.Post;
 
 namespace Branch.API.Features.PingMe
 {
@@ -11,21 +9,25 @@ namespace Branch.API.Features.PingMe
     [ApiController]
     public class PingMeController : ControllerBase
     {
-        [HttpGet("DoEcho")]
-        public IActionResult DoEcho()
+        private readonly IMediator _mediator;
+
+        public PingMeController(IMediator mediator)
         {
-            return Ok("Hello!!! DoEcho");
+            _mediator = mediator;
         }
+
+        [HttpGet("DoEcho")]
+        public ActionResult DoEcho() {
+            return Ok("Hello World!!!");
+        } 
 
         [HttpPost("DoEcho")]
-        public IActionResult DoEcho(DoEchoRequest request)
+        public async Task<IActionResult> DoEcho(DoEchoRequest request)
         {
-            return Ok($"Hello!!! {request.Value}");
+            var result = await _mediator.Send(request);
+            return Ok(result);
         }
     }
 
-    public class DoEchoRequest
-    {
-        public string Value { get; set; }
-    }
+    
 }
---
+++ Branch.API/Features/PingMe/Post/DoEcho.cs
@@ -0,0 +1,39 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using FluentValidation;
+
+namespace Branch.API.Features.PingMe.Post
+{
+
+    public class DoEchoRequest : IRequest<DoEchoResponse>
+    {
+        public string Value { get; set; }
+    }
+
+    public class DoEchoResponse
+    {
+        public string Result { get; set; }
+    }
+
+    public class DoEchoValidator : AbstractValidator<DoEchoRequest>
+    {
+        public DoEchoValidator()
+        {
+            RuleFor(x => x.Value).NotEmpty();
+        }
+    }
+
+    public class DoEchoHandler : IRequestHandler<DoEchoRequest, DoEchoResponse>
+    {
+        public string Value { get; set; }
+
+        public async Task<DoEchoResponse> Handle(DoEchoRequest request, CancellationToken cancellationToken)
+        {
+            return new DoEchoResponse()
+            {
+                Result = $"Hello World!!! {request.Value}"
+            };
+        }
+    }
+}
